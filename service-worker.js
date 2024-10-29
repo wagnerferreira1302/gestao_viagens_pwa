d5ae5bc0f488876b862763cac00663bc10ec792c
felipebarroso <felipe.queiroz.barroso@gmail.com>
2024-10-28 23:52:11 -0300

    correcao instalacao pwd

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js').then(registration => {
+        navigator.serviceWorker.register('./service-worker.js').then(registration => {
             console.log('Service Worker registrado com sucesso:', registration);
         }).catch(error => {
             console.log('Falha ao registrar o Service Worker:', error);
